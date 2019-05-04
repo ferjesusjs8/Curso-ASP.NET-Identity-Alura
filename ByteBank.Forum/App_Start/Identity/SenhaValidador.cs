f252cc3029ccac57ed1e0953d836e642ba95377f
fernando.js8 <fernando.js8@outlook.com>
2019-05-04 11:03:04 -0300

    Initial Commit

---
+++ ByteBank.Forum/App_Start/Identity/SenhaValidador.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNet.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace ByteBank.Forum.App_Start.Identity
+{
+    public class SenhaValidador : IIdentityValidator<string>
+    {
+        public int TamanhoMinimo { get; set; }
+        public bool CaracteresEspeciais { get; set; }
+        public bool LowerCase { get; set; }
+        public bool UpperCase { get; set; }
+        public bool Digitos { get; set; }
+        public async Task<IdentityResult> ValidateAsync(string item)
+        {
+            var erros = new List<string>();
+            if (CaracteresEspeciais && !VerificaCaracteresEspeciais(item))
+                erros.Add("A senha deve conter pelo menos 1 caracter especial!");
+            if (UpperCase && !VerificaUpperCase(item))
+                erros.Add("A senha deve conter pelo menos 1 letra maiúscula!");
+            if (LowerCase && !VerificaLowerCase(item))
+                erros.Add("A senha deve conter pelo menos 1 letra minúscula!");
+            if (Digitos && !VerificaDigitos(item))
+                erros.Add("A senha deve conter pelo menos 1 número!");
+
+            if (erros.Any())
+                return IdentityResult.Failed(erros.ToArray());
+
+            return IdentityResult.Success;
+        }
+
+        private bool VerificaTamanhoMinimo(string senha) => senha?.Length >= TamanhoMinimo;
+
+        private bool VerificaCaracteresEspeciais(string senha) => Regex.IsMatch(senha, @"[~`!@#$%^&*()+=|\\{}':;.,<>/?[\]""_-]");
+
+        private bool VerificaLowerCase(string senha) => senha.Any(char.IsLower);
+
+        private bool VerificaUpperCase(string senha) => senha.Any(char.IsUpper);
+
+        private bool VerificaDigitos(string senha) => senha.Any(char.IsNumber);
+    }
+}
---
+++ ByteBank.Forum/Controllers/ContaController.cs
@@ -0,0 +1,73 @@
+﻿using ByteBank.Forum.Models;
+using ByteBank.Forum.ViewModels;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.Owin;
+using Microsoft.AspNet.Identity.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ByteBank.Forum.Controllers
+{
+    public class ContaController : Controller
+    {
+        private UserManager<UsuarioAplicacao> _userManager;
+        public UserManager<UsuarioAplicacao> UserManager
+        {
+            get
+            {
+                if (_userManager == null)
+                {
+                    var contextOwin = HttpContext.GetOwinContext();
+                    _userManager = contextOwin.GetUserManager<UserManager<UsuarioAplicacao>>();
+                }
+                return _userManager;
+            }
+            set
+            {
+                _userManager = value;
+            }
+        }
+        public ActionResult Registrar()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> Registrar(ContaRegistrarViewModel modelo)
+        {
+            if (ModelState.IsValid)
+            {
+
+                var novoUsuario = new UsuarioAplicacao();
+
+                novoUsuario.UserName = modelo.UserName;
+                novoUsuario.Email = modelo.Email;
+                novoUsuario.NomeCompleto = modelo.NomeCompleto;
+
+                var usuario = UserManager.FindByEmail(modelo.Email);
+
+                var resultado = await UserManager.CreateAsync(novoUsuario, modelo.Senha);
+
+                if (resultado.Succeeded || usuario != null)
+                    return RedirectToAction("Index", "Home");
+                else
+                    AdicionaErros(resultado);
+            }
+            else
+            {
+
+            }
+            return View(modelo);
+        }
+
+        private void AdicionaErros(IdentityResult resultado)
+        {
+            foreach (var erro in resultado.Errors)
+                ModelState.AddModelError("", erro);
+        }
+    }
+}
